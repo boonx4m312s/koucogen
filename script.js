f19f48dadd8bbd7815bc4cfb9721e3aab714d9a7
mejizo <whiteye8780@gmail.com>
2024-11-18 13:28:30 +0900

    create html

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello, JavaScript!");
+
+// ボタンをクリックしたときの処理
+const myButton = document.getElementById("myButton");
+myButton.addEventListener("click", function() {
+  alert("ボタンがクリックされました！");
+});
