3ed0b4a8c4105b2e61ee6f5eb4d60456b8ddbd3a
mejizo <whiteye8780@gmail.com>
2024-12-10 15:57:11 +0900

    update

--- script.js
+++ script.js
@@ -119,7 +119,11 @@ function calculation() {
   document.getElementById("tspan18-5-3-7-49").textContent = `小中勾 ${g.toFixed(9)}`;
   document.getElementById("tspan18-5-3-7-4").textContent = `欠勾 ${h.toFixed(9)}`;
   document.getElementById("text18-0-3").textContent = `勾配 ${θ.toFixed(9)} / 100`;
-
+  
+  const myTable = document.getElementById("table_koucogen");
+  const cell = myTable.rows[1].cells[1];
+  cell.textContent =co.toFixed(9)
+  
   // document.getElementById("text9").textContent = `欠勾隅勾 ${l.toFixed(9)}`;
   // document.getElementById("text9-2").textContent = `隅欠勾 ${m.toFixed(9)}`;
   // document.getElementById("text9-1").textContent = `平垂木栓勾配 ${o.toFixed(9)}`;
